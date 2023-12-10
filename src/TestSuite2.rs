4baebc8f97ef146b83c1f18dfd909083e19d67f8
Alexandru Bara <alexandrubara2000@gmail.com>
2023-12-10 17:12:08 -0500

    Fixed test suite 2

--- src/TestSuite2.rs
+++ src/TestSuite2.rs
@@ -61,10 +61,13 @@ pub(crate) fn test2() {
 }
 
 pub(crate) fn test3() {
+    let mut value_arr = vec![0; NUM_ASSIGNMENTS];
     // Start the timer
     let timer = Timer::start_timer();
 
-    let mut value_arr = vec![0; NUM_ASSIGNMENTS];
+    for i in 0..NUM_ASSIGNMENTS {
+        value_arr.push(i);
+    }
 
     // Stop the timer and calculate the elapsed time
     let elapsed_seconds = timer.get_time_seconds();
@@ -79,16 +82,17 @@ pub(crate) fn test3() {
 
 pub(crate) fn test4() {
     let mut value: usize;
+    let mut value_arr: Vec<Box<usize>> = vec![Box::new(0); NUM_ASSIGNMENTS / 4];
 
     // Start the timer
     let timer = Timer::start_timer();
 
-    let mut value_arr: Vec<Option<usize>> = vec![None; NUM_ASSIGNMENTS / 4];
+
 
     // Perform the assignments
     for i in 0..NUM_ASSIGNMENTS {
         value = i;
-        value_arr[i % 4] = Some(value);
+        value_arr[i % 4] = Box::new(value);
     }
 
     // Stop the timer and calculate the elapsed time
@@ -145,10 +149,13 @@ pub(crate) fn test6() {
 }
 
 pub(crate) fn test7() {
+    let mut value_arr = vec![0f64; NUM_ASSIGNMENTS];
     // Start the timer
     let timer = Timer::start_timer();
 
-    let mut value_arr = vec![0f64; NUM_ASSIGNMENTS];
+    for i in 0..NUM_ASSIGNMENTS {
+        value_arr.push(i as f64);
+    }
 
     // Stop the timer and calculate the elapsed time
     let elapsed_seconds = timer.get_time_seconds();
@@ -163,16 +170,16 @@ pub(crate) fn test7() {
 
 pub(crate) fn test8() {
     let mut value: usize;
-
+    let mut value_arr: Vec<Box<f64>> = vec![Box::new(0.0); NUM_ASSIGNMENTS / 4];
     // Start the timer
     let timer = Timer::start_timer();
 
-    let mut value_arr: Vec<Option<f64>> = vec![None; NUM_ASSIGNMENTS / 4];
+
 
     // Perform the assignments
     for i in 0..NUM_ASSIGNMENTS {
         value = i;
-        value_arr[i % 4] = Some(value as f64);
+        value_arr[i % 4] = Box::new(value as f64);
     }
 
     // Stop the timer and calculate the elapsed time
